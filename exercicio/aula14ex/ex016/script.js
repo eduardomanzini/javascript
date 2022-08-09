ad6d19079fb9cc6d3103f9df5c3a048232a900e7
Eduardo Manzini Carneiro <108757623+eduardomanzini@users.noreply.github.com>
2022-08-09 09:45:54 -0300

    treinando javascript

--- exercicio/aula14ex/ex016/script.js
+++ exercicio/aula14ex/ex016/script.js
@@ -1,1 +1,33 @@
-alert('ola')
+function contar() {
+    let c
+    let inicio = document.getElementById("initxt")
+    let fim = document.getElementById("fimtxt")
+    let passo = document.getElementById("passtxt")
+    let res = document.getElementById("res")
+    
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        res.innerHTML = "Impossivel contar!"
+        //alert('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando: <br> '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if ( p <= 0) {
+            alert('Passo Invalido! Considera Passo 1')
+            p = 1
+        }
+        if (i < f) {
+            //CONTAGEM CRESCENTE
+           for (c = i; c <= f; c += p) {
+              res.innerHTML += `${c} \u{1F449} `
+        } 
+     } else {
+         // CONTATEM REGRESSIVA
+        for (c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1F449} `
+        }
+     }
+     res.innerHTML += `\u{1F3C1}`  
+    }
+}
